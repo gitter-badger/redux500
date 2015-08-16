6e6ab34530a589dbca761e8237e3d74a1f638a7b
Shawn Lim <shawnlim89@live.com>
2015-08-16 17:52:17 +0800

    break up router.js, reorganize into /router

--- src/client/main.js
+++ src/client/main.js
@@ -4,14 +4,14 @@ import React from "react";
 import BrowserHistory from "react-router/lib/BrowserHistory";
 import Location from "react-router/lib/Location";
 import createStore from "../redux/create";
-import router from "../router";
+import createRouter from "../router/create-router";
 
 const history = new BrowserHistory();
 const store = createStore(window.__INITIAL_DATA__);
 const location = new Location(document.location.pathname, document.location.search);
 const dest = document.getElementById("content");
 
-router(location, history, store)
+createRouter(location, history, store)
   .then(({component}) => {
     React.render(component, dest);
   }, (error) => {
---
+++ src/router/create-router.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Router from "react-router";
+import createRoutes from "./create-routes";
+import { Provider } from "react-redux";
+import createTransitionHook from "./create-transition-hook";
+
+export default function createRouter(location, history, store) {
+  const routes = createRoutes(store);
+
+  // this allows us to us to run any kind of hooks before routing,
+  // catches any kind of transition problems,
+  // before resolving with the route handlers to be rendered on the server and client side
+
+  return new Promise((resolve, reject) => {
+    Router.run(routes, location, [createTransitionHook(store)], (error, initialState, transition) => {
+      if (error) {
+        return reject(error);
+      }
+
+      if (transition && transition.redirectInfo) {
+        return resolve({
+          transition,
+          isRedirect: true
+        });
+      }
+
+      if (history) {  // only on client side
+        initialState.history = history;
+      }
+
+      const component = (
+        <Provider store={store} key="provider">
+          {() => <Router {...initialState} children={routes}/>}
+        </Provider>
+      );
+
+      return resolve({
+        component,
+        isRedirect: false
+      });
+    });
+  });
+}

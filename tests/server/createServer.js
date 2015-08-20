7ed54b1349dbff75aed250a4c66036cce890a3eb
Giampaolo Bellavite <io@gpbl.org>
2015-08-20 20:18:38 +0200

    Test express server

---
+++ tests/server/createServer.js
@@ -0,0 +1,30 @@
+import request from "supertest";
+import createServer from "../../src/server/createServer";
+
+describe("createServer", () => {
+  let server;
+
+  beforeEach((done) => {
+    server = createServer(() => {
+      done();
+    });
+
+  });
+
+  afterEach((done) => {
+    server.close(done);
+  });
+
+  it("responds to /", (done) => {
+    request(server)
+      .get("/")
+      .expect(200, done);
+  });
+
+  it("responds to /featured/upcoming", (done) => {
+    request(server)
+      .get("/")
+      .expect(200, done);
+  });
+
+});

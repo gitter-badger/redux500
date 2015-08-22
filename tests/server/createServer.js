e62df53a8193103355ccaf827209788ff06cb404
Giampaolo Bellavite <io@gpbl.org>
2015-08-22 09:09:03 +0200

    Add route not found test

--- tests/server/createServer.js
+++ tests/server/createServer.js
@@ -18,16 +18,15 @@ describe("createServer", () => {
     server.close(done);
   });
 
-  it("responds to /", (done) => {
-    request(server)
-      .get("/")
-      .expect(200, done);
-  });
-
-
   it("serves favicon.ico", (done) => {
     request(server)
       .get("/favicon.ico")
+      .expect(200, done);
+  });
+
+  it("responds to /", (done) => {
+    request(server)
+      .get("/")
       .expect(200, done);
   });
 
@@ -35,6 +34,12 @@ describe("createServer", () => {
     request(server)
       .get("/")
       .expect(200, done);
+  });
+
+  it("responds with 404 to unexisting routes", (done) => {
+    request(server)
+      .get("/foo/bar")
+      .expect(404, done);
   });
 
 });

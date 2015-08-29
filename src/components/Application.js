c0299b331e9d9813ffed4458f1acb2fa5e3d9781
Giampaolo Bellavite <io@gpbl.org>
2015-08-29 09:07:24 +0200

    Add sample components

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,21 +1,30 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "redux-universal-router";
 
 @connect(state => ( { router: state.router } ))
-
 class Application extends Component {
 
   render() {
 
     const { router } = this.props;
-    const { currentRoute } = router;
+    const { currentRoute, nextRoute } = router;
     const Handler = currentRoute.config.handler;
     return (
       <div>
-        Current route is: { router.currentRoute.name }
+        <Link href="/photo/4000">TEST</Link>
+        { " " } <Link href="/notfound">not found</Link>
+        { " " } <Link href="/ok">ok page</Link>
+        { " " } <Link href="/error">error page</Link>
+        { nextRoute && <span> loading route { nextRoute.name }...</span> }
+
+        <p>
+          Current route is? { currentRoute.name }
+        </p>
         <div>
           <Handler {...currentRoute.params } />
         </div>
+
       </div>
     );
   }
---
+++ src/components/ErrorPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NotFoundPage extends Component {
+  render() {
+    return (
+      <div>
+        Error page (this page should not appear)
+      </div>
+    );
+  }
+}
+
+export default NotFoundPage;

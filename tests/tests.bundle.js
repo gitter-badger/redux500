c497736669a86094c366932a24e2b7e171cf5e63
Shawn Lim <shawnlim89@live.com>
2015-08-30 12:56:45 +0800

    set up karma and test files

---
+++ tests/tests.bundle.js
@@ -0,0 +1,11 @@
+// If you use webpack-specific features in your modules (e.g., loaders, plugins) you will need to use webpack to build a test bundle. The fastest and simplest approach is to create a single, test-specific entry file.
+
+var context = require.context(
+  ".",            // context folder
+  true,           // include subdirectories
+  /.+\.spec\.js/  // file pattern to match
+);
+
+context.keys().forEach(context);
+
+module.exports = context;
